5e85159e024bb38ef101d99185b04e4f71e6dde3
gard2np <gard2np@naver.com>
2025-08-01 09:50:30 +0900

    update

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,48 +6,42 @@ export function Welcome() {
   return (
     <div className="w-4/5 mx-auto">
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold">공동사용 밸브 안내</h1>
+        <p className="text-2xl font-bold mb-10">공동사용 밸브 안내</p>
         <div className="flex flex-col gap-4 w-full max-w-md mt-10">
         <Accordion type="single" collapsible className="w-full max-w-md" defaultValue="item-1">
           <AccordionItem value="item-1">
-            <AccordionTrigger className="font-bold text-lg">공동사용 밸브란 무엇인가요?</AccordionTrigger>
+            <AccordionTrigger className="font-bold text-xl">공동사용 밸브란 무엇인가요?</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-4 text-balance">
               <p>지하층에 가스가 공급되는 건물에 지상차단밸브가 없는 경우, 대신 사용하는 도시가스사의 인입밸브입니다.</p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger className="font-bold text-lg">건물 관리인이 할 일은 무엇인가요?</AccordionTrigger>
+            <AccordionTrigger className="font-bold text-xl">건물 관리인이 할 일은 무엇인가요?</AccordionTrigger>
             <AccordionContent className="flex flex-col text-balance">
-              <ul className="my-6 ml-6 list-decimal [&>li]:mt-1">
-                <li>도시가스 인입밸브 위치 숙지</li>
-                <li>도시가스 인입밸브 OPEN / CLOSE 방법 숙지</li>
-                <li>도시가스 교육 이수</li>
-                <li>관리인 변경 시 해당 내용 인수인계</li>
-                <li>화재 발생 시 도시가스 상황실에 연락</li>
-              </ul>
+              <p>도시가스사에 문의하여 해당 건물에 공급되는 가스를 차단할 수 있는 인입밸브의 위치를 확인하시고 밸브 조작법을 교육받으셔야 합니다.</p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">  
-            <AccordionTrigger className="font-bold text-lg">관련 법령은 무엇인가요?</AccordionTrigger>
+            <AccordionTrigger className="font-bold text-xl">관련 법령은 무엇인가요?</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-4 text-balance">
-              <p>도시가스 사업법의 하위 규정인 『KGS CODE FU551 (사용시설의 시설 기술 검사) FU551 2.4.4.1 지하공급 차단밸브』 에 따라 도시가스의 밸브를 사용동의 요청하시고 교육을 받으셔야 합니다.</p>
+              <p>도시가스 사업법의 하위 규정인『KGS CODE FU551(사용시설의 시설 기술 검사) FU551 2.4.4.1 지하공급 차단밸브』에 따라 도시가스의 밸브를 사용동의 요청하시고 교육을 받으셔야 합니다.</p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-4">
-            <AccordionTrigger className="font-bold text-lg">비상연락망</AccordionTrigger>
+            <AccordionTrigger className="font-bold text-xl">비상연락망</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-4 text-balance">
               <a href="tel:041-530-1900">
-                <Button className="w-full">
+                <Button className="w-full text-lg">
                   JB 상황실에 전화하기
                 </Button>
               </a>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-5">
-            <AccordionTrigger className="font-bold text-lg">밸브 위치 확인하기</AccordionTrigger>
+            <AccordionTrigger className="font-bold text-xl">밸브 위치 확인하기</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-4 text-balance">
             <div className="w-full max-w-md mt-2">
-          <GoogleMapIframe lat={37.416} lng={127.2527145} />
+          <GoogleMapIframe lat={36.806866} lng={127.106311} />
             </div>
             </AccordionContent>
           </AccordionItem>
